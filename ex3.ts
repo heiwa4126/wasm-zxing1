ff62bfd2e08dfb349cb583ff59d52b63aa1abee1
heiwa4126 <heiwa4126@gmail.com>
2024-08-30 18:01:19 +0900

    update ex3.ts (add text)

--- ex3.ts
+++ ex3.ts
@@ -26,13 +26,12 @@ const imageFile = await loadFileAsBlob("./examples/qr2.jpg");
 const results = await readBarcodesFromImageFile(imageFile, readerOptions);
 
 const s1 = sharp(Buffer.from(await imageFile.arrayBuffer()));
-// const { width, height } = await s1.metadata();
-// const draw = SVG().size(width, height);
+const { width, height } = await s1.metadata();
+const draw = SVG().size(width, height); // サイズは指定しておかないとクリップされる可能性がある
 // draw
 // 	.circle(50)
 // 	.fill("#f00")
 // 	.center(width / 2, height / 2);
-const draw = SVG();
 
 results.forEach((result, index) => {
 	console.log(`${index + 1}: ${result.text}`);
@@ -42,6 +41,19 @@ results.forEach((result, index) => {
 	const p = result.position;
 	const pathData = `M${p.topLeft.x},${p.topLeft.y} L${p.topRight.x},${p.topRight.y} L${p.bottomRight.x},${p.bottomRight.y} L${p.bottomLeft.x},${p.bottomLeft.y} Z`;
 	draw.path(pathData).fill("none").stroke({ color: "#a00a", width: 10 });
+	const centerX = (p.topLeft.x + p.topRight.x + p.bottomRight.x + p.bottomLeft.x) / 4;
+	const centerY = (p.topLeft.y + p.topRight.y + p.bottomRight.y + p.bottomLeft.y) / 4;
+	draw
+		.text(result.text)
+		.font({
+			family: "Consolas, Menlo, monospace",
+			size: 30,
+			anchor: "middle",
+			leading: "2.0em",
+		})
+		.fill("#0f0")
+		.cx(centerX)
+		.cy(centerY);
 });
 
 s1.composite([{ input: Buffer.from(draw.svg()), top: 0, left: 0 }])
